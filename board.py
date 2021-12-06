b56636dd77d645aeaed84a20db6bef7a68df7139
boyreverend <jbranch2@my.waketech.edu>
2021-12-05 22:37:54 -0500

    Updated functions for game

--- board.py
+++ board.py
@@ -1,5 +1,19 @@
+import random
+board=[['-','-','-'],['-','-','-'],['-','-','-']]
 def print_board():
-  board=[['-','-','-'],['-','-','-'],['-','-','-']]
   print(*board,sep="\n")
   #(*board,sep="\n") is used to separate list elements on new lines
+def whogoesfirst(player):
+  player=random.randint(1,2)
+  print("Player ",player," goes first.")
+  if player == 1:
+    return 1
+  else:
+    return 2
+def game():
+  print_board()
+  
+
 print_board()
+whogoesfirst(player)
+game()
